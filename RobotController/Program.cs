92f48ab63722f60095ac9408937444e453aaf4fb
Dan Shields <d.shieldsuk@gmail.com>
2017-03-29 04:39:43 +0100

    No longer depends on SharpX (Windows only). Uses XInputDotNot instead

--- RobotController/Program.cs
+++ RobotController/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO.Ports;
-using System.Security.AccessControl;
-using SharpDX.XInput;
+using XInputDotNetPure;
+using System.Threading;
 
 namespace RobotController
 {
@@ -11,110 +11,85 @@ namespace RobotController
         {
             bool[] controls = new bool[8];
 
-            SerialPort ser = new SerialPort();
-            ser.BaudRate = 9600;
-            ser.PortName = "COM7";
-            ser.Open();
+            //SerialPort ser = new SerialPort();
+            //ser.BaudRate = 9600;
+            //ser.PortName = "COM7";
+            //ser.Open();
 
-            Console.WriteLine("Start XGamepadApp");
-            // Initialize XInput
-            var controllers = new[] { new Controller(UserIndex.One), new Controller(UserIndex.Two), new Controller(UserIndex.Three), new Controller(UserIndex.Four) };
-
-            // Get 1st controller available
-            Controller controller = null;
-            foreach (var selectControler in controllers)
+           
+            // Poll events from joystick
+            while (true)
             {
-                if (selectControler.IsConnected)
+                var gamepad = GamePad.GetState(PlayerIndex.One);
+
+                int turnSpeed = (int) (gamepad.ThumbSticks.Left.X * 255);
+                bool left = false;
+                bool right = false;
+                bool headingForward = true;
+                bool move = false;
+
+                //Correct turnSpeed
+                if (turnSpeed < 0)
                 {
-                    controller = selectControler;
-                    break;
-                }
-            }
+                    turnSpeed++;
+                    turnSpeed *= -1;
 
-            if (controller == null)
-            {
-                Console.WriteLine("No XInput controller installed");
-            }
-            else
-            {
-
-                Console.WriteLine("Found a XInput controller available");
-                Console.WriteLine("Press buttons on the controller to display events or escape key to exit... ");
-
-                // Poll events from joystick
-                while (controller.IsConnected)
+                    if (turnSpeed > 30)
+                    {
+                        left = true;
+                    }
+                } else if (turnSpeed > 30)
                 {
-                    var state = controller.GetState();
-                    Gamepad gamepad = state.Gamepad;
-
-                    
-
-                    int turnSpeed = gamepad.LeftThumbX / 128;
-                    bool left = false;
-                    bool right = false;
-                    bool headingForward = true;
-                    bool move = false;
-
-                    //Correct turnSpeed
-                    if (turnSpeed < 0)
-                    {
-                        turnSpeed++;
-                        turnSpeed *= -1;
-
-                        if (turnSpeed > 30)
-                        {
-                            left = true;
-                        }
-                    } else if (turnSpeed > 30)
-                    {
-                        right = true;
-                    }
-
-
-                    int globalSpeed = Math.Abs(gamepad.LeftTrigger - gamepad.RightTrigger);
-
-                    if (gamepad.LeftTrigger > gamepad.RightTrigger)
-                    {
-                        headingForward = false;
-                    }
-
-                    if (turnSpeed > 60 || globalSpeed > 0)
-                    {
-                        move = true;
-                    }
-
-                    controls[0] = move;
-                    controls[1] = headingForward;
-                    controls[2] = left;
-                    controls[3] = right;
-                    controls[4] = controls[5] = controls[6] = controls[7] = false;;
-
-                    byte controlByte = 0;
-                    int index = 0;
-
-                    // Loop through the array
-                    foreach (bool b in controls)
-                    {
-                        // if the element is 'true' set the bit at that position
-                        if (b)
-                            controlByte |= (byte)(1 << (7 - index));
-
-                        index++;
-                    }
-
-                    char controlChar = Convert.ToChar(controlByte);
-
-                    int controlInt = Convert.ToInt16(controlChar);
-
-                    Console.WriteLine(controlInt.ToString());
-                    Console.WriteLine(turnSpeed.ToString());
-
-                    ser.WriteLine(controlInt.ToString());
-                    ser.WriteLine(turnSpeed.ToString());
+                    right = true;
                 }
+
+
+                int globalSpeed = (int) ((gamepad.Triggers.Right - gamepad.Triggers.Left) * 255);
+
+                if (globalSpeed < 0)
+                {
+                    headingForward = false;
+                    globalSpeed *= -1;
+                }
+
+                if (turnSpeed > 60 || globalSpeed > 0)
+                {
+                    move = true;
+                }
+
+                controls[0] = move;
+                controls[1] = headingForward;
+                controls[2] = left;
+                controls[3] = right;
+                controls[4] = controls[5] = controls[6] = controls[7] = false;
+
+                byte controlByte = 0;
+                int index = 0;
+
+                // Loop through the array
+                foreach (bool b in controls)
+                {
+                    // if the element is 'true' set the bit at that position
+                    if (b)
+                        controlByte |= (byte)(1 << (7 - index));
+
+                    index++;
+                }
+
+                char controlChar = Convert.ToChar(controlByte);
+
+                int controlInt = Convert.ToInt16(controlChar);
+
+                Console.WriteLine(controlInt.ToString());
+                Console.WriteLine(turnSpeed.ToString());
+                Console.WriteLine(globalSpeed.ToString());
+
+                Thread.Sleep(500);
+
+                //ser.WriteLine(controlInt.ToString());
+                //ser.WriteLine(turnSpeed.ToString());
+                //set.WriteLine(globalSpeed.ToString());
             }
-            Console.WriteLine("End XGamepadApp");
-            ser.Close();
         }
     }
 }
